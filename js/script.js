361db369370bf59a4d4a9f1e3d317df68d746a73
Plexus Network Team <turkey.plexus.system@gmail.com>
2022-10-20 15:43:41 +0300

    :star: updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+function animation() {
+  document.getElementById('iframe').src = "loading.html";
+}
 function change_address(data) {
-
+  setTimeout(animation, 3000);
+  setTimeout();
   document.getElementById('iframe').src = data;
 }
